76fc0282943050227268e80edc3e2e18b36b5996
rockethelll <rockethell@hotmail.com>
2025-07-01 20:06:20 +0200

    Remove unused app files and update test structure

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,18 +1,5 @@
-import { App } from './app';
-import { render, screen } from '@testing-library/angular';
-import userEvent from '@testing-library/user-event';
-import { beforeEach} from '@jest/globals'
-
-describe('App', () => {
-  beforeEach(async () => {
-    await render(App);
-  });
-
-  describe('.showMessage()', () => {
-    it('should update the message when called', async () => {
-      const button = screen.getByRole('button', { name: /show message/i });
-      await userEvent.click(button);
-      expect(await screen.findByText(/I am the new message/i)).toBeTruthy();
-    });
+describe('Test', () => {
+  it('Test lambda', () => {
+    expect(true).toBe(true);
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -6,18 +6,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css',
 })
 export class App {
-  isMessage = false;
-  message = '';
-
-  private getNewMessage() {
-    return 'I am the new message';
-  }
-
-  showMessage() {
-    this.message = this.getNewMessage();
-    this.isMessage = true;
-  }
 }
