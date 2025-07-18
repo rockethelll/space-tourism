d6c4c3d351a6ec06e7dc5322cf8a2c4b12fe1721
rockethelll <51137055+rockethelll@users.noreply.github.com>
2025-07-18 12:55:48 +0000

    Build: (c8fb5af) Add GitHub Actions workflow for build and deployment

---
+++ space-tourism-website-project/browser/chunk-4LAHLYJK.js
@@ -0,0 +1,3 @@
+import{Ma as n,Na as c,Sa as l,ua as t}from"./chunk-6EE7M22N.js";var g=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-technology"]],decls:2,vars:0,consts:[[1,"bg-[url('/images/technology/background-technology-mobile.jpg')]","md:bg-[url('/images/technology/background-technology-tablet.jpg')]","lg:bg-[url('/images/technology/background-technology-desktop.jpg')]","bg-cover","bg-center","min-h-screen","h-full","p-6","pt-22"]],template:function(e,i){e&1&&(n(0,"div",0),l(1,`
+Technology
+`),c())},encapsulation:2})};export{g as Technology};
---
+++ space-tourism-website-project/browser/chunk-Z7PLCYOY.js
@@ -0,0 +1,3 @@
+import{Ma as n,Na as c,Sa as o,ua as r}from"./chunk-6EE7M22N.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=r({type:t,selectors:[["app-crew"]],decls:2,vars:0,consts:[[1,"bg-[url('/images/crew/background-crew-mobile.jpg')]","md:bg-[url('/images/crew/background-crew-tablet.jpg')]","lg:bg-[url('/images/crew/background-crew-desktop.jpg')]","bg-cover","bg-center","min-h-screen","h-full","p-6","pt-22"]],template:function(e,s){e&1&&(n(0,"div",0),o(1,`
+Crew
+`),c())},encapsulation:2})};export{a as Crew};
